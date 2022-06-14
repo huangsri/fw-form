fe41e82b05ff4704a01a6cb3af30b826ddf740a5
huangsri <peerawas.likit@gmail.com>
2022-06-14 14:42:43 +0700

    init project

---
+++ src/lib/theme/index.tsx
@@ -0,0 +1,58 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Global } from '@emotion/react'
+
+import { layerStyles } from './layerStyles'
+import { textStyles } from './textStyles'
+
+import components from './components'
+import colors from './colors'
+
+const overrideTheme = extendTheme({
+  colors,
+  components,
+  layerStyles,
+  textStyles,
+  fonts: {
+    heading: 'Kanit, sans-serif',
+    body: 'sans-serif',
+  },
+  styles: {
+    global: {
+      body: {},
+    },
+  },
+})
+
+export default overrideTheme
+
+export const ThemeProvider = (props: { children: React.ReactNode }) => {
+  return (
+    <ChakraProvider resetCSS theme={overrideTheme}>
+      <Global
+        styles={`
+          @font-face {
+            font-display: swap;
+            font-family: 'Kanit';
+            font-style: normal;
+            font-weight: 400;
+          }
+
+          @font-face {
+            font-display: swap;
+            font-family: 'Kanit';
+            font-style: normal;
+            font-weight: 500;
+          }
+
+          @font-face {
+            font-display: swap;
+            font-family: 'Kanit';
+            font-style: normal;
+            font-weight: 600;
+          }
+        `}
+      />
+      {props.children}
+    </ChakraProvider>
+  )
+}
