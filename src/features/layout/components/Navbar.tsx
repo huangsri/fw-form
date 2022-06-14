9430128acd0d33b50935842a32e1b45ddd941186
huangsri <peerawas.likit@gmail.com>
2022-06-14 22:58:03 +0700

    feat: search with query

--- src/features/layout/components/Navbar.tsx
+++ src/features/layout/components/Navbar.tsx
@@ -1,23 +1,41 @@
 import { Box, Flex, HStack, Image, Input, Link } from '@chakra-ui/react'
+import { useForm } from 'react-hook-form'
 
 import { navLinks } from '../constants'
 
 export const Navbar = () => {
+  const { register, handleSubmit } = useForm<{ query: string }>()
+
   return (
     <Box as="nav" sx={{ px: 8, py: 4, pr: 12, bg: 'brand.600' }}>
       <Flex sx={{ justifyContent: 'space-between' }}>
         <HStack spacing="8">
-          <Image alt="fastwork logo" src="/images/logo.svg" />
+          <Image alt="fastwork logo" src="/logo.svg" />
 
-          <Input
-            sx={{
-              w: '400px',
-              bg: 'white',
-              borderRadius: 'full',
-              fontSize: 'xs',
-            }}
-            placeholder="จ้างฟรีแลนซ์ทำ..."
-          />
+          <form
+            onSubmit={handleSubmit((data) => {
+              if (!data.query) return
+
+              window.open(
+                `https://staging.fastwork.co/search?q=${data.query}`,
+                '_blank'
+              )
+            })}
+          >
+            <Input
+              sx={{
+                w: '400px',
+                bg: 'white',
+                borderRadius: 'full',
+                fontSize: 'xs',
+                _placeholder: {
+                  color: 'gray.800',
+                },
+              }}
+              placeholder="จ้างฟรีแลนซ์ทำ..."
+              {...register('query')}
+            />
+          </form>
         </HStack>
 
         <HStack spacing="8" sx={{ color: 'white' }} textStyle="heading">
